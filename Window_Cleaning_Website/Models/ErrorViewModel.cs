4968574cd8f97475ee3ffdae7e13421c311b2a57
Micah <mjmikes@byu.edu>
2025-02-03 08:34:08 -0700

    Framework

---
+++ Window_Cleaning_Website/Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace Window_Cleaning_Website.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
